ac1222045c503173c7a0dec32edd401111e1227d
Zeyu Yao <novodoodle@gmail.com>
2023-01-11 23:24:32 +0800

    Minor edits to description and marquee content

--- src/App.js
+++ src/App.js
@@ -13,12 +13,12 @@ function App() {
           <div className="letter-container">
             <p className="App-description">Greetings IJHS fellows! We hope you are doing well.</p>
             <p className="App-description">
-              We want to share a big thank you for one of the <span className="hover-underline-animation">180+ scholars</span> from <span className="hover-underline-animation">14+ countries</span> in our 5th-ever YALA Camp, also the first in-person camp since the pandemic in 2022.
+              We want to share a big thank you for being one of the <span className="hover-underline-animation">180+ scholars</span> from <span className="hover-underline-animation">more than 14 countries</span> in our 5th-ever YALA Camp, also the first in-person camp since the pandemic in 2022.
               We enjoyed hearing all of your great ideas and seeing all of you blossom into great young leaders throughout the camp - we really hope you enjoyed it as well!
             </p>
             <p className="App-description">
               Throughout the camp, we took <span className="hover-underline-animation">a lot of pictures</span> about the various activities and events that happened, and we want to share them with you.
-              Thanks again for joining us, and we hope to see you again <span className="hover-underline-animation">in 2023!</span>
+              Thanks again for joining us, and we hope to <span className="hover-underline-animation">see you again in 2023!</span>
             </p>
             <p className="App-author">
               - The IJHS Global Student Leadership Council (GSLC)
@@ -41,7 +41,7 @@ function App() {
           <Marquee velocity={12}>
             {times(10, Number).map(id => (
               <p key={id} className="App-rolling-text-content">
-                Thanks a lot for joining us, and we hope to see you again in 2023!
+                Thanks a lot for joining us for YALA Camp 2022!
               </p>
             ))}
           </Marquee>
